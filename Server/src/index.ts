640cde68e97910e4f5c63cb463a015401f6a5bfa
Abhishek <abhishektiwari@gmail.com>
2026-01-08 18:24:34 +0530

    backend issues

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -14,13 +14,14 @@ const PORT = process.env.PORT;
 // CORS configuration
 app.use(cors({
   origin: [
+    'https://booking-system25.vercel.app',
     'https://booking-syatem-2507.vercel.app',
     'http://localhost:5173',
   ],
-  credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
+
 
 
 
